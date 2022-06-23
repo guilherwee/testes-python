1d68f65cd5fb8102e43d4795a4d7412f5b0f1f12
Guilherme Fernandes <guilherme.araujo@datenworks.com>
2022-06-23 13:45:28 -0300

    fix

--- ex/ex017.py
+++ ex/ex017.py
@@ -1,4 +1,4 @@
 catop = int(input('Comprimento do cateto oposto: '))
 catad = int(input('Comprimento do cateto adjacente: '))
 hip = (catop*catop)+(catop*catad)
-print(f'o valor da hipotenusa é {hip}')
+print(f'o valor da hipotenusa é {hip}M')
